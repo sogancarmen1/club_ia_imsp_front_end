f5076975a2ac1f5437832ce293b573d778a4165a
carmen <sogancarmen1@gmail.com>
2025-02-14 12:45:06 +0100

    delete middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("Authorization")?.value;
- console.log("Token in middleware:", token);
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("Authorization")?.value;
+//   console.log("Token in middleware:", token);
 
-  if (!token) {
-    console.log("No token, redirecting to /auth/signin"); 
-    return NextResponse.redirect(new URL("/auth/signin", req.url));
-  }
+//   if (!token) {
+//     console.log("No token, redirecting to /auth/signin"); 
+//     return NextResponse.redirect(new URL("/auth/signin", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/admin/:path*", "/forms/:path*"],
-};
+// export const config = {
+//   matcher: ["/admin/:path*", "/forms/:path*"],
+// };
