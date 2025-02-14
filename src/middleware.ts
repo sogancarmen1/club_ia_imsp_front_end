4aed515b1b11f60e5a1111a07cc48136296544d3
carmen <sogancarmen1@gmail.com>
2025-02-14 15:12:52 +0100

    get token from backend 123

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("Authorization")?.value;
+  console.log("Token in middleware:", token);
+
+  if (!token) {
+    console.log("No token, redirecting to /auth/signin"); 
+    return NextResponse.redirect(new URL("/auth/signin", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*", "/forms/:path*"],
+};
