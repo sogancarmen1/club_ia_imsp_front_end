8ae44a7883d078c6c0fb325a73e09a710bf4f96d
carmen <sogancarmen1@gmail.com>
2025-02-15 18:04:50 +0100

    regularisation

--- src/components/Tables/TableThree.tsx
+++ src/components/Tables/TableThree.tsx
@@ -9,9 +9,8 @@ import { useEffect, useState } from "react";
 
 const TableThree = () => {
   // const token = cookies().get("Authorization")?.value;
-  const { role } = useDashboard();
   const [allEditor, setAllEditor] = useState<any>();
-  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const [role, setRole] = useState<any>();
 
   useEffect(() => {
     const value = async () => {
@@ -28,7 +27,7 @@ const TableThree = () => {
             { withCredentials: true },
           );
           setAllEditor(allEditors.data?.data);
-          setIsAdmin(true);
+          setRole(true);
         }
       } catch (error) {}
     };
@@ -36,12 +35,9 @@ const TableThree = () => {
     value();
   }, []);
 
-  // Condition pour déterminer si le composant doit être affiché
-  const shouldRender = role && role !== "admin"; // Exemple de condition
-  // console.log(role);
-  // Si la condition n'est pas remplie, ne pas afficher le composant
+  const shouldRender = role && role !== "admin";
   if (shouldRender) {
-    return <p></p>; // Vous pouvez personnaliser ce message
+    return <p></p>;
   }
 
   const handleSubmit = async (id: string) => {
@@ -62,24 +58,23 @@ const TableThree = () => {
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
-          {isAdmin && (
-            <thead>
-              <tr className="bg-gray-2 text-left dark:bg-meta-4">
-                <th className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
-                  Email éditeurs
-                </th>
-                <th className="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white">
-                  Date d'inscription
-                </th>
-                <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
-                  Etat compte
-                </th>
-                <th className="px-4 py-4 font-medium text-black dark:text-white">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-          )}
+          <thead>
+            <tr className="bg-gray-2 text-left dark:bg-meta-4">
+              <th className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
+                Email éditeurs
+              </th>
+              <th className="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white">
+                Date d'inscription
+              </th>
+              <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                Etat compte
+              </th>
+              <th className="px-4 py-4 font-medium text-black dark:text-white">
+                Actions
+              </th>
+            </tr>
+          </thead>
+
           <tbody>
             {allEditor
               ?.slice()
